7e015aaa0c6512f5a8220339c6600f4b3b7c29b1
qhoeriltila <qhoeriltila@gmail.com>
2024-08-22 04:31:04 +0000

    FlutterGetX-Crud

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,32 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const COUNTER = _Paths.COUNTER;
+  static const BIODATA = _Paths.BIODATA;
+  static const REGISTER = _Paths.REGISTER;
+  static const LOGIN = _Paths.LOGIN;
+  static const PROFILE = _Paths.PROFILE;
+  static const BOTTOM_MENU = _Paths.BOTTOM_MENU;
+  static const KATEGORI = _Paths.KATEGORI;
+  static const CREATE_KATEGORI = _Paths.CREATE_KATEGORI;
+  static const EDIT_KATEGORI = _Paths.EDIT_KATEGORI;
+  static const SHOW_KATEGORI = _Paths.SHOW_KATEGORI;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const COUNTER = '/counter';
+  static const BIODATA = '/biodata';
+  static const REGISTER = '/register';
+  static const LOGIN = '/login';
+  static const PROFILE = '/profile';
+  static const BOTTOM_MENU = '/bottom-menu';
+  static const KATEGORI = '/kategori';
+  static const CREATE_KATEGORI = '/kategori/create';
+  static const EDIT_KATEGORI = '/kategori/edit';
+  static const SHOW_KATEGORI = '/kategori/show';
+}
